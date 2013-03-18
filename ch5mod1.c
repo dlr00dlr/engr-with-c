22a7e298b15715de77929b975e69aefee6e10869
dlr00dlr <dlr00dlr@gmail.com>
2013-03-17 20:26:50 -0500

    sort and search

--- ch5mod1.c
+++ ch5mod1.c
@@ -3,6 +3,23 @@ int main(void)
 {	int k; double x[10]={4,8,-2,16,19,6,-4,0,20,3};
 	void sort(double x[], int n);
 	
-	printf();
-
+	printf("Original Order \n");
+	for (k=0;k<=9;k++) printf ("%.1f ",x[k]);
+	printf("\n");
+	sort(x,10);
+	printf("New Order\n");
+	for(k=0;k<=9;k++) printf ("%.1f ",x[k]);
+	printf("\n");
+	return 0;
+}
+void sort(double x[], int npts)
+{	int k, j, m; double hold;
+	for (k=0; k<=npts-2; k++)
+	{	m=k; for (j=k+1; j<=npts-1; j++)
+			if (x[j] < x[m]) m=j;
+		hold = x[m];
+		x[m] = x[k];
+		x[k] = hold;
+	}
+	return;
 }
